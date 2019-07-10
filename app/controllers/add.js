2076e321e874166d18d3b46a6e53e1e75244dcd9
Lucas Merat <lucas.merat@gmail.com>
2019-07-10 09:59:13 -0500

    Auto redirect on home. Small styling. Add item redirect

--- app/controllers/add.js
+++ app/controllers/add.js
@@ -1,15 +1,17 @@
 import Controller from '@ember/controller';
-
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
     titleValue: "",
+    router: service(),
     actions: {
-        addItem() {
+        async addItem() {
            let route = this.target.currentRoute.parent.localName;
            let singularRoute = this.target.currentRoute.parent.localName.substr(0, route.length-1);
            const item = this.store.createRecord(singularRoute, { title: this.titleValue });
-           item.save();
+           await item.save();
            this.set('titleValue', "");
+           this.router.transitionTo("add.books.book", item.id)
         }
     },
 });
